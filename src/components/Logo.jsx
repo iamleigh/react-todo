9509e5ae6b793cb4c00a95480eb479899b339681
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-05 17:40:10 -0500

    feat(logo): Create new component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+	return (
+		<div class="lq-logo lq-logo--center">
+			<div class="lq-logo__icon" aria-hidden="true">
+				<span class="fa-solid fa-rocket"></span>
+			</div>
+
+			<h1 class="lq-logo__name">Rocket Games</h1>
+		</div>
+	);
+}
+
+export { Logo }
