f84f3abbdab650e765227584a0d45d1e92be24a2
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-05 17:40:03 -0500

    feat(footer): Create new component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<footer className="lq-footer">
+			<p>Designed & Developed by <a href="https://github.com/iamleigh" rel="noreferrer">Leighton Quito</a></p>
+		</footer>
+	);
+}
+
+export { Footer }
