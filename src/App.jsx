8e41ecf58be51f9e2b9c1f55ee8f8fd0f26de6de
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-05 17:44:55 -0500

    fix: Update main structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Logo } from './components/Logo';
+import { Reminders } from './components/Reminders';
+import { Footer } from './components/Footer';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<>
+			<section>
+				<Logo />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+				<Reminders />
+			</section>
+
+			<Footer />
+		</>
+	)
 }
 
 export default App
