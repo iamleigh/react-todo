c394460a124f00dba49c8c8496a133a1e3ee28c9
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-05 17:44:14 -0500

    fix(logo): Incorrect attribute

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,11 @@
 const Logo = () => {
 	return (
-		<div class="lq-logo lq-logo--center">
-			<div class="lq-logo__icon" aria-hidden="true">
-				<span class="fa-solid fa-rocket"></span>
+		<div className="lq-logo lq-logo--center">
+			<div className="lq-logo__icon" aria-hidden="true">
+				<span className="fa-solid fa-rocket"></span>
 			</div>
 
-			<h1 class="lq-logo__name">Rocket Games</h1>
+			<h1 className="lq-logo__name">Rocket Games</h1>
 		</div>
 	);
 }
