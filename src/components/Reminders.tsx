cb7f6c3b78c5e5b967afa738e2ec892f42a65a4a
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-05 22:19:10 -0500

    fix(reminders): Add `ListItem` component on button click

--- src/components/Reminders.tsx
+++ src/components/Reminders.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { ListItem } from "./ListItem";
 
 const Reminders: React.FC = () => {
+	const [items, setItems] = useState<number[]>([]);
+
+	const addItem = () => {
+		const newItem = Date.now();
+		setItems( v => [ ...v, newItem ] );
+	}
+
 	return (
 		<div className="lq-reminders">
 			<div className="lq-reminders__insert">
-				<input />
-				<button>Add New</button>
+				<input type="text" />
+				<button onClick={ addItem }>Add New</button>
 			</div>
 
 			<ul className="lq-reminders__list">
-				<li>Item 1 <button>Remove</button></li>
+				{ items.map( () => <ListItem name="text" /> ) }
 			</ul>
 		</div>
 	);
