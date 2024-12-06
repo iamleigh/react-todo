21655d71da2896255e316301f7ed9ef5caa318f9
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-06 18:53:11 -0500

    fix(reminders): Improve component
    - Add todos the `done` prop. This will help handle if an item is completed.
    - Add “Clear Completed” button to remove all items alread completed.
    - Pass `todo.done` to component `done` prop.
    - Pass `completeItem` func to `checkHandler` prop.
    - Add `completeItem` func to update todo item keys.

--- src/components/Reminders.tsx
+++ src/components/Reminders.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { ListItem } from "./ListItem";
 
 const Reminders: React.FC = () => {
-	const [todos, setTodos] = useState<{ id: number, label: string }[]>([]);
+	const [todos, setTodos] = useState<{ id: number, label: string, done: boolean }[]>([]);
 	const [counter, setCounter] = useState<number>(1);
 	const [todoLabel, setTodoLabel] = useState<string>('');
 
@@ -12,7 +12,7 @@ const Reminders: React.FC = () => {
 			// Add new item to the list
 			setTodos( ( v ) => [
 				...v,
-				{ id: counter, label: todoLabel }
+				{ id: counter, label: todoLabel, done: false }
 			]);
 
 			// Increment the counter
@@ -24,11 +24,30 @@ const Reminders: React.FC = () => {
 	}
 
 	const removeItem = ( e: React.MouseEvent<HTMLButtonElement> ) => {
-		const button = e.target as HTMLButtonElement;
-		const index = button.getAttribute( 'data-item-id' );
+		const button = e.currentTarget as HTMLButtonElement;
+		const parent = button.parentElement;
+		const index = null !== parent ? parent.getAttribute( 'data-item-id' ) : null;
 
-		if ( null !== index && '' !== index ) {
+		if ( null !== index ) {
 			const updatedTodos = todos.filter( todo => todo.id !== parseInt( index ) );
+			setTodos( updatedTodos );
+		}
+	}
+
+	const completeItem = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+		const input = e.currentTarget as HTMLInputElement;
+		const parent = input.parentElement;
+		const index = null !== parent ? parent.getAttribute( 'data-item-id' ) : null;
+
+		if ( null !== index ) {
+			const todoIndex = todos.findIndex( todo => todo.id === parseInt( index ) );
+			const updatedTodos = [ ...todos ];
+
+			updatedTodos[ todoIndex ] = {
+				...updatedTodos[ todoIndex ],
+				done: input.checked,
+			};
+
 			setTodos( updatedTodos );
 		}
 	}
@@ -37,6 +56,11 @@ const Reminders: React.FC = () => {
 		if ( 'Enter' === e.key ) {
 			addItem();
 		}
+	}
+
+	const handleClear = () => {
+		const updatedTodos = todos.filter( todo => todo.done !== true );
+		setTodos( updatedTodos );
 	}
 
 	const handleInputChange = ( e: { target: { value: React.SetStateAction<string>; }; } ) => {
@@ -57,10 +81,14 @@ const Reminders: React.FC = () => {
 							key={ index }
 							name={ todo.label }
 							itemId={ todo.id }
+							done={ todo.done }
+							checkHandler={ completeItem }
 							clearHandler={ removeItem } />
 					);
 				}) }
 			</ul>
+
+			<button onClick={ handleClear }>Clear Completed</button>
 		</div>
 	);
 }
