fef14e1cbda361decf2006d77a09f24baa7ded0d
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-05 21:34:01 -0500

    fix(footer): Convert to TS component
    - Update file extension.
    - Update code to match TS function compoent.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
-const Footer = () => {
+import React from 'react';
+
+const Footer: React.FC = () => {
 	return (
 		<footer className="lq-footer">
 			<p>Designed & Developed by <a href="https://github.com/iamleigh" rel="noreferrer">Leighton Quito</a></p>
