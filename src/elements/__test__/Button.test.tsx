a8a0b86d2e30cbc2dd10e3908ceb79cc521ed7bf
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-12 20:25:48 -0500

    feat(button): Test component with JEST

---
+++ src/elements/__test__/Button.test.tsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Button } from '../Button';
+
+test( 'Button: Rendering with required props', () => {
+	render( <Button label="Hello Button" /> );
+	expect( true ).toBeTruthy();
+});
+
+test( 'Button: Pass onClick prop when clicked', () => {
+	const onClick = jest.fn();
+	render( <Button label="Hello Button" onClick={ onClick } /> );
+	fireEvent.click( screen.getByText( /Hello Button/i ) );
+	expect( onClick ).toHaveBeenCalledTimes(1);
+});
