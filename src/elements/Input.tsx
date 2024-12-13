95dd16a536def2446fbd786baa90e69027d03545
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-12 22:30:33 -0500

    feat(input): Create new element
    - Create `Input` element with basic props.
    - Limit `type` prop allowed list.
    - Setup element test scenarios:
      - Limit `type` defaulting “checkbox” and “radio” to “text”.
      - Correctly pass a value.
      - Correctly render the element.

---
+++ src/elements/Input.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	type?: string
+}
+
+const Input: React.FC<InputProps> = ({ type, ...props }) => {
+	let inputType;
+
+	switch ( type ) {
+		case 'text':
+		case 'tel':
+		case 'email':
+		case 'number':
+		case 'password':
+			inputType = type;
+			break;
+
+		default:
+			inputType = 'text';
+			break;
+	}
+
+	return (
+		<input
+			type={ inputType }
+			className="lq-input"
+			{ ...props } />
+	);
+}
+
+export { Input }
---
+++ src/elements/__test__/Input.test.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Input } from '../Input';
+
+test( 'Input: Render element', () => {
+	render ( <Input /> );
+	expect( true ).toBeTruthy();
+});
+
+test( 'Input: Pass value', () => {
+	const onChange = jest.fn();
+	render( <Input defaultValue="Hello Input" data-testid="input-test" onChange={ onChange } /> );
+	expect( screen.getByTestId( 'input-test' ) ).toHaveValue( 'Hello Input' );
+});
+
+test( 'Input: Limit type options', () => {
+	render( <Input type="radio" data-testid="input-test-radio" /> );
+	const radioInput = screen.getByTestId( 'input-test-radio' );
+	expect( radioInput ).toHaveProperty( 'type', 'text' );
+
+	render( <Input type="checkbox" data-testid="input-test-checkbox" /> );
+	const checkInput = screen.getByTestId( 'input-test-checkbox' );
+	expect( checkInput ).toHaveProperty( 'type', 'text' );
+});
