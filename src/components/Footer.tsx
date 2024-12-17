cffd7e588bf61c8b491c6fc905fbfa07f9926bdf
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-17 15:54:05 -0500

    fix(footer): Update footer styles
    - Update footer class name prefix from “lq-“ to “rocketui-“
    - Load new styles for footer block

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
 	return (
-		<footer className="lq-footer">
+		<footer className="rocketui-footer">
 			<p>Designed & Developed by <a href="https://github.com/iamleigh" rel="noreferrer" target="_blank">Leighton Quito</a></p>
 		</footer>
 	);
